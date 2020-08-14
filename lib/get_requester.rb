0e038bf8ef68b5e8e5d1d9b331dc8ff1af57cb60
Anthony Lepore <violacellonyc@gmail.com>
2020-08-14 14:03:29 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,8 +5,8 @@ require 'json'
 
 class GetRequester
   
-  def get_response_body(url)
- uri = URI.parse(url)
+  def get_response_body(URL)
+ uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   
