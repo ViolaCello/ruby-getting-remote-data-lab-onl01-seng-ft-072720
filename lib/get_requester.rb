9b262a562d3e9d2ca6fb3bb5a615cc55c2b0ef6c
Anthony Lepore <violacellonyc@gmail.com>
2020-08-14 13:55:30 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,7 +1,10 @@
 # Write your code here
 class GetRequester
   
+  def get_response_body(url)
+ 
   
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+end
+  
   
 end
