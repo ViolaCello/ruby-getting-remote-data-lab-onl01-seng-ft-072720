efa386781c8392d01740d9c0647d1f5c42c33f4a
Anthony Lepore <violacellonyc@gmail.com>
2020-08-14 13:33:25 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,6 +1,7 @@
 # Write your code here
 class GetRequester
   
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
 end
